cb5850d8857bb5656372b6a3f4d7f04e53f457af
孙敏 <sunmin@MacBook-Pro.local>
2021-11-07 23:34:16 +0800

    img

--- src/components/global.vue
+++ src/components/global.vue
@@ -4,7 +4,7 @@ const list={
         "1":  {
           bjimh: require("@/assets/img/xzBj1.png"),
           icon: require("@/assets/img/xunzhaung1.png"),
-          name: "Force Badge",
+          name: "Power Badge",
           info:"Buried in the depths of the ancient volcanic mountains in the Alps, with the appearance of angels, some stones began to show strange elemental reactions. When the elves passed by these rocks with turquoise light, the inside of the stones would burst into explosions. Sound. Function: It can incubate a powerful offensive angel.",
           level:1,
           countinfo:1900 
@@ -28,7 +28,7 @@ const list={
          "4":  {
           bjimh: require("@/assets/img/xzBj4.png"),
           icon: require("@/assets/img/xunzhaung4.png"),
-          name: "Time Badge",
+          name: "Time badge",
            level:4,
           countinfo:467,
           info:"It exists in the deepest cave in the magic forest of Angel Valley, waiting for adventurers to collect it. Function: It can incubate rare functional angels, can increase the time of team battles, and control wild monsters to join the battle."
@@ -36,7 +36,7 @@ const list={
          "5":  {
           bjimh: require("@/assets/img/xzBj5.png"),
           icon: require("@/assets/img/xunzhaung5.png"),
-          name: "Heart Badge",
+          name: "Mind Badge",
           level:5,
           countinfo:216,
           info:"Existing in the qualified adventurers of Angel Valley, in order to strengthen the ecological development of Angel Valley, these adventurers have contributed their beloved gems. Function: Be able to incubate angels with rare attributes, get additional combat rewards, participate in the ranking contest for entering Angel Valley, and take priority in participating in various activities organized by Angel Valley."
